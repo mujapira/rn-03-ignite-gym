047892c04668f7d985bb3655a04b3a27ef0d3f7f
mujapira <malmasmello2@gmail.com>
2023-08-21 18:54:06 -0300

    Input component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
 import { Loading } from "@components/loading";
@@ -10,10 +10,10 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+  
         <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
-        {fontsLoaded ? <Text>Oi mundo</Text> : <Loading />}
+        {fontsLoaded ? <SignIn/> : <Loading />}
    
     
     </NativeBaseProvider>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,15 +1,34 @@
-import { Image, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 import BackgroundImage from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg={'gray.700'}>
+    <VStack flex={1} bg={"gray.700"} px={10}>
       <Image
         source={BackgroundImage}
         alt="Imagem de pessoas treinando na academia"
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo.
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse a conta
+        </Heading>
+
+        <Input placeholder="E-mail" keyboardType="email-address" autoCapitalize="none" />
+        <Input placeholder="Senha" secureTextEntry />
+      </Center>
     </VStack>
   );
 }
