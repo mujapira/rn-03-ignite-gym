da7efacf7d292bad96c822c2a03b9ce5f4a52be0
mujapira <malmasmello2@gmail.com>
2023-08-23 09:48:44 -0300

    Home header done

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,16 +1,19 @@
-import { Heading, HStack, Text, VStack } from 'native-base';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
 import { UserPhoto } from './UserPhoto';
+import { TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export function HomeHeader() {
   return (
-    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center" >
       <UserPhoto 
         source={{ uri: 'https://github.com/mujapira.png' }}
         size={16}
         alt="Imagem do usuário"
         mr={4}
       />
-      <VStack>
+
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
@@ -19,6 +22,15 @@ export function HomeHeader() {
           Mujapira
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   );
 }
