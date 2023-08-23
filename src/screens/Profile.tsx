dba636b2a3b6351b4854f2d4256d88315d12fd15
mujapira <malmasmello2@gmail.com>
2023-08-23 18:16:26 -0300

    Change user picture functionality

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,80 +1,90 @@
-import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading, useTheme } from 'native-base';
+import { useState } from "react";
+import { TouchableOpacity, Alert } from "react-native";
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, Toast } from "native-base";
 
-import { ScreenHeader } from '@components/ScreenHeader';
-import { UserPhoto } from '@components/UserPhoto';
-import { Input } from '@components/Input';
-import { Button } from '@components/Button';
-
-
-const PHOTO_SIZE = 33;
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from 'expo-file-system';
 
 export function Profile() {
-
+  const PHOTO_SIZE = 33;
+  const [userPhoto, setUserPhoto] = useState("https://github.com/mujapira.png");
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
+  async function handleUserPhotoSelected() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+        if (photoInfo.exists && (photoInfo.size / 1024 / 1024 > 5)) {
+          return Toast.show({
+            title: 'A imagem deve ter no máximo 5MB',
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
-      <ScreenHeader title='Perfil' />
+      <ScreenHeader title="Perfil" />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
-          {
-            photoIsLoading ?
-              <Skeleton 
-                w={PHOTO_SIZE}
-                h={PHOTO_SIZE}
-                rounded="full"
-                startColor="gray.500"
-                endColor="gray.400"
-              />
-            :
-              <UserPhoto 
-                source={{ uri: 'https://github.com/mujapira.png' }}
-                alt="Foto do usuário"
-                size={PHOTO_SIZE}
-              />
-          }
-          
-          <TouchableOpacity>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto source={{ uri: userPhoto }} alt="Foto do usuário" size={PHOTO_SIZE} />
+          )}
+
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar Foto
             </Text>
           </TouchableOpacity>
 
-          <Input 
-            bg="gray.600" 
-            placeholder='Nome' 
-          />
+          <Input bg="gray.600" placeholder="Nome" />
 
-          <Input 
-            bg="gray.600" 
-            placeholder="E-mail"
-            isDisabled
-          />
-        
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
+
           <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={8}>
             Alterar senha
           </Heading>
 
-          <Input 
-            bg="gray.600"
-            placeholder="Senha antiga"
-            secureTextEntry
-          />
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
 
-          <Input 
-            bg="gray.600"
-            placeholder="Nova senha"
-            secureTextEntry
-          />
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
 
-          <Input 
-            bg="gray.600"
-            placeholder="Confirme a nova senha"
-            secureTextEntry
-          />
+          <Input bg="gray.600" placeholder="Confirme a nova senha" secureTextEntry />
 
           <Button title="Atualizar" mt={4} />
         </Center>
