d612fd61be631756424d8f072663cb39017cbc98
mujapira <malmasmello2@gmail.com>
2023-08-23 11:39:57 -0300

    Listing groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,44 @@
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { FlatList, HStack, Heading, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("costa");
+  const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "ombro"]);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
