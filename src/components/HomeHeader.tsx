19dfb4cf3d82f4c832278368a864ee715151309e
mujapira <malmasmello2@gmail.com>
2023-08-22 21:07:02 -0300

    Bottom navigator

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,17 @@
+import { Heading, HStack, Text, VStack } from 'native-base';
+
+export function HomeHeader() {
+  return (
+    <HStack>
+      <VStack>
+        <Text color="gray.100">
+          Olá,
+        </Text>
+
+        <Heading color="gray.100">
+          Maurílio
+        </Heading>
+      </VStack>
+    </HStack>
+  );
+}
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -9,6 +9,7 @@ import { Home } from "@screens/Home";
 import { Exercise } from "@screens/Exercise";
 import { History } from "@screens/History";
 import { Profile } from "@screens/Profile";
+import { Platform } from "react-native";
 
 type AppRoutes = {
   home: undefined;
@@ -32,6 +33,13 @@ export function AppRoutes() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? 'auto' : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6]
+        }
       }}
     >
       <Screen
@@ -58,7 +66,7 @@ export function AppRoutes() {
         }}
       />
 
-      <Screen name="exercise" component={Exercise} />
+      <Screen name="exercise" component={Exercise} options={{ tabBarButton: () => null }} />
     </Navigator>
   );
 }
