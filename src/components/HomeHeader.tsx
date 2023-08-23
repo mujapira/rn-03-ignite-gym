bfd74f9d8f64addb0e34799464bd44c44fab2afa
mujapira <malmasmello2@gmail.com>
2023-08-23 09:40:55 -0300

    Home header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,14 +2,14 @@ import { Heading, HStack, Text, VStack } from 'native-base';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
-          Maurílio
+        <Heading color="gray.100" fontSize="md">
+          Mujapira
         </Heading>
       </VStack>
     </HStack>
