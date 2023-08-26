5f428a0608aea1812a97e88171fb1fd52492efd3
mujapira <malmasmello2@gmail.com>
2023-08-26 11:03:04 -0300

    Registering a new user

--- MOBILE/src/screens/SignUp.tsx
+++ MOBILE/src/screens/SignUp.tsx
@@ -43,8 +43,18 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm });
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    const response = await fetch("http://192.168.0.50:3333/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
@@ -67,7 +77,7 @@ export function SignUp() {
         </Center>
 
         <Center>
-          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading" >
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
 
